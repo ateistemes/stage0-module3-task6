043b4a1f79d719361f56ce650dc5bf6b2911d55f
ateistemes <timur.tynychbekov@gmail.com>
2023-10-13 22:46:38 +0600

    A

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        int var = Math.round(numberToBeRounded);
+        System.out.println(var);
     }
 }
